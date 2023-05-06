702a9ace81b9f15242debef5a5af819a56638b73
St1ll1984 <still100584@gmail.com>
2023-05-06 21:31:44 +0300

    start page, add note, bootstrap, navmenu

--- src/main/java/com/example/list/config/controller/NoteController.java
+++ src/main/java/com/example/list/config/controller/NoteController.java
@@ -1,27 +1,43 @@
 package com.example.list.config.controller;
 
+import com.example.list.note.Note;
 import com.example.list.note.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
-@RequestMapping("/note")
 @Controller
 public class NoteController {
     private final NoteService noteService;
 
-    @GetMapping("/list")
+    @GetMapping("/note/list")
     public ModelAndView getAllNotes() {
         ModelAndView result = new ModelAndView("note/list");
         result.addObject("noteList", noteService.getAll());
         return result;
     }
-    @PostMapping("/delete")
+    @PostMapping("/note/delete")
     public String deleteNote(@RequestParam("id") long id) {
         noteService.deleteById(id);
         return "redirect:/note/list";
     }
 
+
+    @GetMapping("/note/add")
+    public ModelAndView add(Model model) {
+//        model.addAttribute("note", new Note());
+//        return "add";
+        ModelAndView result = new ModelAndView("note/add");
+      //  result.addObject("noteList", noteService.getAll());
+        return result;
+    }
+
+    @PostMapping("/note/add")
+    public String add(@ModelAttribute Note note) {
+        noteService.add(note);
+        return "redirect:/note/list";
+    }
 }
