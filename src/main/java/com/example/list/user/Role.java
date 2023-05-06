edcc0647c305d8d93e06e11596ab750abd526a98
Dmrchk <105112928+Dmrchk@users.noreply.github.com>
2023-05-06 22:49:10 +0200

    Add files via upload

---
+++ src/main/java/com/example/list/user/Role.java
@@ -0,0 +1,15 @@
+package user;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
